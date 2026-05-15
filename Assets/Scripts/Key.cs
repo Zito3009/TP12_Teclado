c98057d37238f59c969f2d4fd64be654ffbc9a94
Zito3009 <riverzito09@gmail.com>
2026-05-15 14:26:26 -0300

    Initial commit

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        Debug.Log("Click en " + gameObject.name);
+    }
+}
